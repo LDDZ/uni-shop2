4c5ae07553b3cbe6d634e39a46fac3fe1c208090
Jie Lin <2603792595@qq.com>
2022-08-12 11:07:31 +0800

    完成了搜索功能的开发

---
+++ components/my-search/my-search.vue
@@ -0,0 +1,65 @@
+<template>
+  <view class="my-search-container" :style="{'background-color':bgColor}" @click="searchBoxHandler">
+    <view class="my-search-box" :style="{'border-radius':radius+'px'}">
+      <!-- 使用 uni-ui 提供的图标组件 -->
+      <uni-icons type="search" size="17"></uni-icons>
+      <text class="placeholder">搜索</text>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name: "my-search",
+    props: {
+      // 背景颜色
+      bgColor: {
+        type: String,
+        default: '#C00000'
+      },
+      // 圆角尺寸
+      radius: {
+        type: Number,
+        // 单位是 px
+        default: 18
+      }
+    },
+    data() {
+      return {
+
+      };
+    },
+    methods: {
+      searchBoxHandler() {
+        this.$emit('click')
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .my-search-container {
+    height: 50px;
+    // 移除背景颜色，改由 props 属性控制
+    // background-color: #C00000;
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+
+    .my-search-box {
+      height: 36px;
+      background-color: #fff;
+      // 移除圆角尺寸，改由 props 属性控制
+      // border-radius: 15px;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .placeholder {
+        font-style: 15px;
+        margin-left: 5px;
+      }
+    }
+  }
+</style>
--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,5 +1,9 @@
 <template>
   <view>
+    <!-- 使用自定义的搜索组件 -->
+    <!-- <my-search :bgColor="'pink'" :radius="4"></my-search> -->
+    <my-search @click="gotoSearch"></my-search>
+
     <view class="scroll-view-container">
       <!-- 左侧的滚动视图区域 -->
       <scroll-view class="left-scroll-view" scroll-y :style="{height: wh+'px'}">
@@ -50,7 +54,8 @@
       const windowInfo = uni.getWindowInfo()
       // console.log(windowInfo);
       // 为 wh 窗口可用高度动态赋值
-      this.wh = windowInfo.windowHeight;
+      // 可用高度 = 屏幕高度 - navigationBar高度 - tabBar高度 - 自定义的search组件高度
+      this.wh = windowInfo.windowHeight - 50;
       // 调用获取分类列表数据的方法
       this.getCateList()
     },
@@ -77,6 +82,11 @@
       gotoGoodsList(item3) {
         uni.navigateTo({
           url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
+        })
+      },
+      gotoSearch(){
+        uni.navigateTo({
+          url:'/subpkg/search/search'
         })
       }
     }
